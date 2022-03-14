859b7e2d2b0b816047814538b481729dda979b00
AbbyKamuri <99327462+AbbyKamuri@users.noreply.github.com>
2022-03-14 21:58:47 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+stderr("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
