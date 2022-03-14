a41c720a031befe7bfc47f50213cad20d10ad0b7
AbbyKamuri <99327462+AbbyKamuri@users.noreply.github.com>
2022-03-14 22:10:10 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-/**
-*main - entry point
-*
-*Return: Always 0 (Success)
-*/
 int main(void)
 {
 fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
