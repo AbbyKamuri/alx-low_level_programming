2a120d8447322f12af88f9328d1be464323c8607
Abby-Kamuri <kamuriabigail@gmail.com>
2022-03-17 19:29:55 -0700

    posive , negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,22 @@
-
+#include <stdlib.h>
+#include<time.h>
+/**
+*main - entry point
+*
+*Return: 1 (FAILURE)
+*/
+int main (void)
+{
+	int n;
+	srand(time(0))
+	if(n > 0){
+	printf(n,"is positive\n");
+	}
+	if(n == 0){
+	printf(n,"is zero\n");
+	}
+	else{
+	printf(n, "is negative\n");
+	}
+	return (0);
+}
