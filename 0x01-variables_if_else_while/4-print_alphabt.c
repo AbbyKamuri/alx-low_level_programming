48fc368519a2f02f814e2cfd4a8db9dfdd56773d
AbbyKamuri <99327462+AbbyKamuri@users.noreply.github.com>
2022-03-19 20:01:07 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,20 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+/**
+ * main - entry point
+ *
+ * Return: 1 (FAILURE)
+ */
+int main(void)
+{
+	char ch = 'a';
 
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+		putchar('\n');
+	return (0);
+}
