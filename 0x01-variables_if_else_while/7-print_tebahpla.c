89829ad60b648f287664ced01d57b25705adb855
Abby-Kamuri <kamuriabigail@gmail.com>
2022-03-19 11:57:06 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - entry point
+ *
+ * Return: 1 (FAILURE)
+ */
+int main(void)
+{
+	int i = 122;
 
+	while (i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
