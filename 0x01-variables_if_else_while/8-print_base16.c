ded299fdff24ea5b0a3b72b91d7f435aa406fcbe
Abby-Kamuri <kamuriabigail@gmail.com>
2022-03-19 12:12:44 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - entry point
+ *
+ * Return: 1 (FAILURE)
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 97;
 
+	while (i >= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	while(i >= 102)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
