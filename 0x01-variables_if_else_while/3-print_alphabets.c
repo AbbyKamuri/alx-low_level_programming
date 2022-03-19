e22c215d987ebe3723a34e4d06532233a7b2d0e9
Abby-Kamuri <kamuriabigail@gmail.com>
2022-03-19 09:47:25 -0700

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - entry point
+ *
+ *Return: 1 (FAILURE)
+*/
+int main(void)
+{
+	char lc = 'a';
+	char uc = 'A';
 
+	while (lc <= 'z')
+	{
+		putchar(lc);
+		lc++;
+	}
+	while (uc <= 'Z')
+	{
+		putchar(uc);
+		uc++;
+	}
+	putchar('\n');
+	return (0);
+}
