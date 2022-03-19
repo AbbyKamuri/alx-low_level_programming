e09e9fa34b1d0e9df03b0d9e190dd3b8fb818c74
AbbyKamuri <99327462+AbbyKamuri@users.noreply.github.com>
2022-03-19 20:36:58 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+*main - entry point
+*
+*Return: 1 (FAILURE)
+*/
+int main (void)
+{
+  int i = 0;
+  
+  while(i < 10)
+  {
+    printf(i);
+  }
+  printf('\n');
+  return (0);
+}
