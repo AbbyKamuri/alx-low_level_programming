a68f28fe1e50064f34afc4beaa5e7bb6febbe699
AbbyKamuri <99327462+AbbyKamuri@users.noreply.github.com>
2022-03-20 17:47:57 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 1 (FAILURE)
+ */
+int main(void)
+{
+	int outer = 48;
+	int inner = 49;
+	int comma = 44;
+	int space = 32;
+	int controller = 49;
 
+	while (outer <= 56)
+	{
+	while(inner <= 57)
+        {
+         putchar(outer);
+         putchar(inner);
+        if (outer != 56 || inner !=57)
+         {
+         putchar(comma);
+         putchar(space);
+         }
+         inner++;
+        }
+	outer++;
+        controller++;
+        inner=controller;
+	}
+	putchar('\n');
+	return (0);
+
+}
