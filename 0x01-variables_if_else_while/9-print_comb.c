15ec3e4040f25de485721712e2e4fc9cf99b887b
Abby-Kamuri <kamuriabigail@gmail.com>
2022-03-20 01:37:00 -0700

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+/**
+ * main - entry point
+ *
+ * Return: 1 (FAILURE)
+ */
+int main(void)
+{
+	int i = 48;
 
+	while (i <= 57)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+
+}
