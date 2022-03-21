3032d4df6d9265e4da15b622d26debad2d8510c1
Abby-Kamuri <kamuriabigail@gmail.com>
2022-03-21 11:44:07 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio>
+/**
+ * main - entry point
+ *
+ * Return: (FAILURE)
+ */
+int main(void)
+{
+putchar(95);
+putchar(112);
+putchar(117);
+putchar(116);
+putchar(99);
+putchar(104);
+putchar(97);
+putchar(114);
+putchar('\n');
+}
