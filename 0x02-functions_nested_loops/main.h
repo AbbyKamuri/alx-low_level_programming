98d20eafaa0458792c5fdbd99d3cd15148c5d849
AbbyKamuri <99327462+AbbyKamuri@users.noreply.github.com>
2022-03-23 00:25:15 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-char alphabets;
-void print_alphabet(void)
-{
 
-alphabets = 'a';
-while (alphabets <= 'z')
-{
-putchar(alphabets);
-alphabets++;
-}
-putchar('\n');
-}
+void print_alphabet(void);
+
 		
