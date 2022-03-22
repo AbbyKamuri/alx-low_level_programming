29036d4079058728d2adeed103f65062574d5f2c
AbbyKamuri <99327462+AbbyKamuri@users.noreply.github.com>
2022-03-23 00:38:10 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,6 +6,12 @@
  *
  * Return: Always 0
  */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
 void print_alphabet(void)
 {
 char alphabets;
@@ -16,9 +22,4 @@ putchar(alphabets);
 alphabets++;
 }
 putchar('\n');
-}
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
