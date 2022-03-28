5703355ae1dcf7416098a264a13c575a41c91c29
AbbyKamuri <99327462+AbbyKamuri@users.noreply.github.com>
2022-03-28 09:34:21 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,16 @@
+#include "main.h"
 
+/**
+* _strlen - return the length of the string
+* @s: char type pointer
+*Return: length
+*/
+
+int _strlen(char *s)
+{
+int c;
+  
+  for (c = 0; s[c] != '\0'; c++)
+    ;
+  return (c);
+}
