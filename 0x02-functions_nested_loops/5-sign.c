569fdf94343fd6bf84741c6f7cf2e1b9516c8da8
Abby Kamuri <kamuriabigail@gmail.com>
2022-06-23 01:49:29 -0700

    print sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,3 +6,22 @@
  *
  * Return: 1 (FAILURE)
  */
+int print_sign(int n){
+	char result;
+	if(n > 0){
+		result = '+';
+		return 1;
+		printf(result);
+}
+else if(n = 0){
+result = 0;
+return 0;
+printf(result);
+}
+else{
+result = '-1';
+return -1;
+printf(result);
+}
+return 0;
+}
